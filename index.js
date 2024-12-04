0394cca9f858e2caae979d4044d11406279a9692
Sun Huaping <106392719+MLeaf-coder@users.noreply.github.com>
2024-12-04 21:54:24 +0800

    Update /help
    Update the /help plugin to improve the adaptive command generated known issues, unable to change the adaptive screenshot height with the size of the web page

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 import kfc from "./kfc/kfc.js";
 import ppx from "./ppx/ppx.js";
+import help from "./help/help.js";
 import log from "#logger";
 import { NewMessage } from "telegram/events/index.js";
 
+export const Notes = {
+  ppx: {
+    usage: "/ppx",
+    description: "解析皮皮虾视频",
+  },
+  kfc: {
+    usage: "/kfc",
+    description: "解析皮皮虾视频",
+  },
+  help: {
+    usage: "/help",
+    description: "解析皮皮虾视频",
+  },
+};
 export default async function (client) {
   // 定义一个处理器函数
   const handler = async (event) => {
@@ -22,6 +37,8 @@ export default async function (client) {
         await ppx(client, event);
       } else if (cmd === "/kfc") {
         await kfc(client, event);
+      } else if (cmd === "/help") {
+        await help(client, event);
       }
     } catch (error) {
       log.error(`[MLeaf-plugins]插件处理消息时出错: ${error}`);
